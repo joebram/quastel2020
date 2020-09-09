8b52197a85d9d592a70fe8a0ca246c11a4167cc5
Joe Bramall <joe@joebramall.com>
2020-09-09 08:16:13 +0100

    Updated logo al text
    Updated logo alt text

--- contactthanks.php
+++ contactthanks.php
@@ -15,7 +15,7 @@
       <nav class="top-bar" data-topbar role="navigation">
         <ul class="title-area">
           <li class="name">
-            <a href="index.html"><img src="img/logo.png" alt="quastel logo" class="logo" /></a>
+            <a href="index.html"><img src="img/logo.png" alt="Quastel Associates" class="logo" /></a>
           </li>
            <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
           <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
